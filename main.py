c721fedf434786a9441d2783b27ba9af28eb5a75
petrynski <mpetrynski@wp.pl>
2021-01-14 01:33:11 +0100

    Added changing charset feature, fixed max output size extreme cases

--- main.py
+++ main.py
@@ -12,13 +12,20 @@ class MainWindow(QMainWindow):
         self.pic = None
         self.scaled_pic = None
         self.gen_im = None
-        self.charset = "#$XKb%,-. "
         self.maxImgWidth = 300;
         super(MainWindow, self).__init__()
         loadUi("asciiConverterMainWindow.ui", self)
 
         self.fileButton.clicked.connect(self.file_open)
+        self.confirmCharSetButton.clicked.connect(self.get_charset)
         self.GenerateBWButton.clicked.connect(self.generate_bw)
+        self.changeCharset("#$XKb%,-. ")
+
+    def get_charset(self):
+        txt = self.charSetInput.text()
+        print(txt)
+        if txt:
+            self.changeCharset(txt)
 
     def file_open(self):
         file_name, _ = QFileDialog.getOpenFileName(self, 'Open File', 'C:\\', 'Image files (*.jpg *.jpeg *.bmp *.png)')
@@ -31,6 +38,11 @@ class MainWindow(QMainWindow):
             text_file = open("Output.txt", "w")
             width, height = self.pic.size
             self.maxImgWidth = int(self.ScaleInput.text())
+            if self.maxImgWidth > 500 or self.maxImgWidth < -1:
+                self.label_7.setStyleSheet("background-color: black; color: red")
+                return
+
+            self.label_7.setStyleSheet("background-color default: black; color: black")
             scale_factor = self.maxImgWidth / width
             self.scaled_pic = self.pic.resize((int(width*scale_factor), int(height*scale_factor*0.5)), Image.NEAREST)
             pix_map = self.scaled_pic.load()
@@ -49,6 +61,9 @@ class MainWindow(QMainWindow):
             pix = QPixmap.fromImage(self.gen_im)
             self.generatedPicture.setPixmap(pix)
 
+    def changeCharset(self, txt):
+        self.charset = txt
+        self.ActiveCharSetLabel.setText("Obecny zestaw znaków: " + '"' + txt + '"')
 
 def window():
     app = QApplication(sys.argv)
